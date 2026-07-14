77484e3c1fc4cdc0a6e02d4754f5158657065dae
Omaraboutahir0 <67918567+Omaraboutahir0@users.noreply.github.com>
2026-07-14 15:20:49 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,29 @@
-This is a test notification from the ntfy iOS app. It has a priority of 3. If you send another one, it may look different.
+import hashlib
+
+import requests
+
+from bs4 import BeautifulSoup
+
+URL = "https://www.fstg-marrakech.ac.ma/"
+
+print("Téléchargement du site...")
+
+response = requests.get(URL, timeout=30)
+
+response.raise_for_status()
+
+print("Analyse de la page...")
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+page_text = soup.get_text(separator=" ", strip=True)
+
+current_hash = hashlib.sha256(
+
+    page_text.encode("utf-8")
+
+).hexdigest()
+
+print("Hash actuel :")
+
+print(current_hash)
