85e817c892bb1b985af5b0a9c2bf58c871f3ba0c
Omaraboutahir0 <67918567+Omaraboutahir0@users.noreply.github.com>
2026-07-14 16:42:26 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,15 +1,16 @@
 import urllib.request
 
-# Configuration exacte selon vos indications :
-# Application/Serveur : FST.sh
-# Topic : FST Marrakech-Marrakech (encodé pour les espaces dans l'URL)
-url = "fstg-marrakech.ac.ma"
+# Configuration de l'adresse avec le protocole sécurisé https:// obligatoire pour Python
+# Nous utilisons ici l'adresse officielle de ntfy avec votre canal (topic) "FSTG-Marrakech"
+url = "https://ntfy.sh/FSTG-Marrakech"
+
+# Message de test
 message = "Le script de surveillance fonctionne à merveille ! 🚀".encode("utf-8")
 
 headers = {
-    "Title": "Test de Notification",
+    "Title": "Test de Notification FST",
     "Priority": "high",
-    "Tags": "white_check_mark,computer"
+    "Tags": "white_check_mark,mortar_board"
 }
 
 req = urllib.request.Request(url, data=message, headers=headers, method="POST")
